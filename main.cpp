07c5cdd627362cf8ddc91d6f89dbfb78426c132d
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-11-25 17:57:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int a=1, b=0, c=0, n=0;
+    cin >> n;
+    if(n>0){
+        cout << a << endl;
+    while(c<n) {
+        c=a+b;
+        b=a;
+        a=c;
+        if (c<=n){
+            cout << c<< endl;
+        }
+    }
+
+    }
    return 0;
 }
